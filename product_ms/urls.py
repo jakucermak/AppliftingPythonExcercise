4b4f75766afe5d9fc756c4c31ae7e851a2fa5cd9
Jakub Čermák <65736749+jakucermak@users.noreply.github.com>
2022-07-12 20:22:54 +0200

    renamed projectname to microservice

--- product_ms/urls.py
+++ product_ms/urls.py
@@ -1,4 +1,4 @@
-"""applifting_python URL Configuration
+"""product_ms URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
